6f700ab06480117602eb05bd23c67f8098d9eac4
Ivo Stefanov <ivostefanovtues@gmail.com>
2022-05-25 09:37:23 +0300

    GPF-1672 fix item add menu

--- src/app/item-add-menu/item-add-menu.component.ts
+++ src/app/item-add-menu/item-add-menu.component.ts
@@ -6,18 +6,16 @@ import { ItemAddEvent } from './item-add-menu';
   templateUrl: './item-add-menu.component.html',
   styleUrls: ['./item-add-menu.component.css']
 })
-export class ItemAddMenuComponent implements OnInit, OnChanges {
+export class ItemAddMenuComponent implements OnChanges {
   @Input() public id: string = '';
   @Input() public items: string[] = [];
   @Output() public addItemEvent = new EventEmitter<ItemAddEvent>();
   public filteredItems: string[] = [];
+  public searchText: string = '';
   public showMenu: boolean = false;
   private isInside = false;
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
   ngOnChanges() {
     this.filteredItems = this.items;
@@ -31,6 +29,7 @@ export class ItemAddMenuComponent implements OnInit, OnChanges {
 
   public resetSearch(): void {
     this.filteredItems = this.items;
+    this.searchText = '';
   }
 
   public addItem(id: string, item: string): void {
--- src/app/users-management/users-management.component.ts
+++ src/app/users-management/users-management.component.ts
@@ -11,12 +11,14 @@ import { UserRow } from './users-management';
 export class UsersManagementComponent implements OnInit {
   public userRows: UserRow[] = [];
   public allGroups: string[] = [];
+  // Primitive used for triggering change detection
+  public manualPipeTrigger = 0;
 
   constructor(
     private managementService: ManagementService
   ) { }
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
     this.managementService.getUserRows().subscribe(rows => {
       this.userRows = rows;
     });
@@ -25,7 +27,16 @@ export class UsersManagementComponent implements OnInit {
     });
   }
 
-  addGroup($event: ItemAddEvent) {
+  public addGroup($event: ItemAddEvent) {
     this.userRows[this.userRows.findIndex(user => user.email === $event.id)].groups.push($event.item);
+  }
+
+  public removeGroup(userRow: UserRow, group: string) {
+    userRow.groups.splice(userRow.groups.indexOf(group), 1);
+    this.manualPipeTrigger++;
+  }
+
+  public deleteUser(userRow: UserRow) {
+    this.userRows.splice(this.userRows.indexOf(userRow), 1);
   }
 }
